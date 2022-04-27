339d0d6ba4f231e10004303b671f039c24b12d4f
prisbe <brianprisbe@gmail.com>
2022-04-27 16:39:46 -0400

    made scripts for deploying to rinkeby

--- scripts/deployToRinkeby.py
+++ scripts/deployToRinkeby.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from brownie import Sealed_Bid_Auction, network, config
 from scripts.helpful_scripts import get_account
 
@@ -5,11 +6,12 @@ from scripts.helpful_scripts import get_account
 def deploy_sealed_bid_auction_contract():
     account = get_account()
     print(f"Current account is: {account.address}")
-    sealed_bid_auction = Sealed_Bid_Auction.deploy({"from": account})
+    sealed_bid_auction = Sealed_Bid_Auction.deploy(
+        {"from": account}, publish_source=TRUE
+    )
     print(f"Deployed Contract to {sealed_bid_auction.address}")
     return sealed_bid_auction
 
 
 def main():
-    tx = deploy_sealed_bid_auction_contract()
-    print(tx)
+    deploy_sealed_bid_auction_contract()
